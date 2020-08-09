65f180d879ece8b53512f390496a230079b03bd8
apc518 <56745633+apc518@users.noreply.github.com>
2020-08-08 19:23:03 -0700

    Added samplerate() and bitdepth()

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-wavparser v0.1.0 by Andy Chamberlain
+pywavparser v0.2.0 by Andy Chamberlain
 
 A simple script that you can use to parse a wav file into a list of lists of floats.
 
-The return value from parse() called channel_data is a list of channels, where
-each channel is a list of floats, where each float represents an audio sample and ranges
-from -1.0 to 1.0
+The return value from parse() is a list of channels, where each channel is a list of floats and each float represents an audio sample and ranges from -1.0 to 1.0
 
-You can pass in a list of the above format to the save() function and optionally add
-a filepath/filename, specify a bit depth, or specify a sample rate.
+You can pass in a list of the above format to the save() function and optionally add a filepath/filename, specify a bit depth, or specify a sample rate.
 
-The save function will make sure the filename saved is unique so it doesn't overwrite
-but if you want to have more control over the save process you can get the bytes
-directly with get_wav_bytes()
+The save function will make sure the filename saved is unique so it doesn't overwrite but if you want to have more control over the save process you can get the bytes directly with get_wav_bytes()
+
+Parsing a wav file loses the sample rate and bit depth information, so there are functions to get those values from a file as well: samplerate() and bitdepth()
+
+Functions:
+
+	parse(filepath)
+	returns: list
+
+	save(audio_data, filepath=None, bitdepth=16, samplerate=44100)
+	returns: void
+
+	samplerate(filepath)
+	returns: int
+
+	bitdepth(filepath)
+	returns: int
--- wavparser.py
+++ wavparser.py
@@ -1,11 +1,10 @@
 """
-wavparser v0.1.0 by Andy Chamberlain
+wavparser v0.2.0 by Andy Chamberlain
 
 A simple script that you can use to parse a wav file into a list of lists of floats.
 
-The return value from parse() called channel_data is a list of channels, where
-each channel is a list of floats, where each float represents an audio sample and ranges
-from -1.0 to 1.0
+The return value from parse() is a list of channels, where each channel is a list of floats
+and each float represents an audio sample and ranges from -1.0 to 1.0
 
 You can pass in a list of the above format to the save() function and optionally add
 a filepath/filename, specify a bit depth, or specify a sample rate.
@@ -13,6 +12,9 @@ a filepath/filename, specify a bit depth, or specify a sample rate.
 The save function will make sure the filename saved is unique so it doesn't overwrite
 but if you want to have more control over the save process you can get the bytes
 directly with get_wav_bytes()
+
+Parsing a wav file loses the sample rate and bit depth information, so there are functions
+to get those values from a file as well: samplerate() and bitdepth()
 """
 
 import os
@@ -22,12 +24,7 @@ class WavFormatException(Exception):
 class ChannelException(Exception):
 	pass
 
-def parse(filepath):
-	"""returns the list of lists of floats which represents the wav file at filepath"""
-	wav_input = open(filepath, "rb")
-	wav_bytes = bytearray(wav_input.read())
-	if wav_bytes[0:4] != bytearray(b'RIFF') or wav_bytes[8:12] != bytearray(b'WAVE'):
-		raise WavFormatException("Inputted file is not a valid wav file.")
+def find_fmt_offset(wav_bytes):
 	fmt_index = 0
 	for i in range(12, len(wav_bytes) - 4):
 		if wav_bytes[i:i+4] == bytearray(b'fmt '):
@@ -35,7 +32,18 @@ def parse(filepath):
 			break
 	if fmt_index == 0:
 		raise WavFormatException("Could not find \"fmt \" subchunk")
-	fmt_offset = fmt_index - 12
+	return fmt_index - 12
+
+def parse(filepath):
+	"""returns the list of lists of floats which represents the wav file at filepath"""
+	wav_file = open(filepath, "rb")
+	wav_bytes = bytearray(wav_file.read())
+	wav_file.close()
+	if wav_bytes[0:4] != bytearray(b'RIFF') or wav_bytes[8:12] != bytearray(b'WAVE'):
+		raise WavFormatException("Inputted file is not a valid wav file.")
+	
+	# because of the possibility of a JUNK chunk, we cannot assume 'fmt ' will be at index 12
+	fmt_offset = find_fmt_offset(wav_bytes)
 	if int.from_bytes(wav_bytes[20+fmt_offset:22+fmt_offset],'little',signed=False) != 1:
 		raise WavFormatException("Invalid audio format. Must be uncompressed wav.")
 	channel_num = int.from_bytes(wav_bytes[22+fmt_offset:24+fmt_offset],'little',signed=False)
@@ -50,23 +58,37 @@ def parse(filepath):
 	wav_data = wav_bytes[44+fmt_offset:44+fmt_offset+data_bytes_num]
 	
 	byps = int(bit_depth / 8) # bytes per sample
-	channel_data = [] # a list of lists, each sublist being a channel as a list of floats
+	audio_data = [] # a list of lists, each sublist being a channel as a list of floats
 	for i in range(0,channel_num):
-		channel_data.append([])
+		audio_data.append([])
 
 	for i in range(0, int(len(wav_data) / (byps * channel_num))):
 		for k in range(0,channel_num):
 			intnum = int.from_bytes(wav_data[i*byps*channel_num + k*byps:i*byps*channel_num + k*byps + byps],'little',signed=True)
-			channel_data[k].append(float(intnum / (2**(bit_depth-1) - 1)))
+			audio_data[k].append(float(intnum / (2**(bit_depth-1) - 1)))
 
-	return channel_data
+	return audio_data
 
-def get_wav_bytes(channel_data, filepath=None, bitdepth=16, samplerate=44100):
-	"""returns a wav file as a bytearray based on the inputted channel_data
-	the channel_data must be a list of lists of floats"""
-	if type(channel_data) != list:
+def samplerate(filepath, bitdepth=False):
+	wav_file = open(filepath, "rb")
+	# read 128 bytes, if the format chunk isnt there then read the whole thing
+	wav_bytes = bytearray(wav_file.read(128))
+	if not(bytearray(b'fmt ') in wav_bytes):
+		wav_bytes += bytearray(wav_file.read())
+	fmt_offset = find_fmt_offset(wav_bytes)
+	if bitdepth:
+		return int.from_bytes(wav_bytes[34+fmt_offset:36+fmt_offset],'little',signed=False)
+	return int.from_bytes(wav_bytes[24+fmt_offset:28+fmt_offset],'little',signed=False)
+
+def bitdepth(filepath):
+	return samplerate(filepath, True)
+
+def get_wav_bytes(audio_data, filepath=None, bitdepth=16, samplerate=44100):
+	"""returns a wav file as a bytearray based on the inputted audio_data
+	the audio_data must be a list of lists of floats"""
+	if type(audio_data) != list:
 		raise ValueError("The argument given was not a list")
-	for lst in channel_data:
+	for lst in audio_data:
 		if type(lst) != list:
 			raise ValueError("The argument give was not a list of lists")
 		for item in lst:
@@ -76,28 +98,28 @@ def get_wav_bytes(channel_data, filepath=None, bitdepth=16, samplerate=44100):
 				raise ValueError("Sublists must contain floats. Found a " + str(type(item)))
 			if not(item >= -1.0 and item <= 1.0):
 				raise ValueError("Values must be in range [-1.0, 1.0]")
-	for lst in channel_data:
-		if len(lst) != len(channel_data[0]):
+	for lst in audio_data:
+		if len(lst) != len(audio_data[0]):
 			raise ChannelException("Not all channels are of equal length.")
 
-	total_samples = len(channel_data) * len(channel_data[0])
+	total_samples = len(audio_data) * len(audio_data[0])
 	
 	start_of_file = bytearray(b'RIFF$\x00\x00\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00D\xac\x00\x00\x10\xb1\x02\x00\x04\x00\x10\x00data')
-	slicesize = int(len(channel_data) * bitdepth / 8)
+	slicesize = int(len(audio_data) * bitdepth / 8)
 	subchunk2size = int(total_samples*bitdepth/8)
 	start_of_file = start_of_file[:4] + int(36+subchunk2size).to_bytes(4,'little',signed=False) + start_of_file[8:]
-	start_of_file = start_of_file[:22] + len(channel_data).to_bytes(2,'little',signed=False) + samplerate.to_bytes(4,'little',signed=False) + int(samplerate * len(channel_data) * bitdepth / 8).to_bytes(4,'little',signed=False) + slicesize.to_bytes(2,'little',signed=False) + bitdepth.to_bytes(2,'little',signed=False) + start_of_file[36:]
+	start_of_file = start_of_file[:22] + len(audio_data).to_bytes(2,'little',signed=False) + samplerate.to_bytes(4,'little',signed=False) + int(samplerate * len(audio_data) * bitdepth / 8).to_bytes(4,'little',signed=False) + slicesize.to_bytes(2,'little',signed=False) + bitdepth.to_bytes(2,'little',signed=False) + start_of_file[36:]
 	start_of_file += subchunk2size.to_bytes(4,'little',signed=False)
 	wav_data = bytearray()
-	for i in range(0, int(total_samples / len(channel_data))):
-		for k in range(0, len(channel_data)):
-			smpint = int(channel_data[k][i] * (2**(bitdepth-1) - 1))
+	for i in range(0, int(total_samples / len(audio_data))):
+		for k in range(0, len(audio_data)):
+			smpint = int(audio_data[k][i] * (2**(bitdepth-1) - 1))
 			wav_data += (smpint.to_bytes(int(bitdepth/8),'little',signed=True))
 	return start_of_file + wav_data
 
-def save(channel_data, filepath=None, bitdepth=16, samplerate=44100):
+def save(audio_data, filepath=None, bitdepth=16, samplerate=44100):
 	"""saves the bytes of a wav file to disk as an actual wav file"""
-	wav_bytes = get_wav_bytes(channel_data, filepath, bitdepth, samplerate)
+	wav_bytes = get_wav_bytes(audio_data, filepath, bitdepth, samplerate)
 
 	if filepath == None:
 		outfile_name = "wavparser_output_"
